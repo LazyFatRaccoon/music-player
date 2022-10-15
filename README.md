6f078b08bef3e2f37d076183c05b7ec9f73d8163
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2022-10-15 18:11:16 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Just player app
---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+//import { useState, useEffect } from 'react';
+
+//import { Notify } from 'notiflix/build/notiflix-notify-aio';
+//import uniqid from 'uniqid';
+
+export default function App() {
+  return (
+    <div>
+      <h1 style={{ margin: '0px' }}>Player</h1>
+    </div>
+  );
+}
